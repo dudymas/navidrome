0a86a7a7be00d4f565882feb375253d2a263c621
Rigel Trajano <rigel@gooee.com>
2020-08-29 23:48:39 -0400

    more stubs

--- scanner/cloud_scanner.go
+++ scanner/cloud_scanner.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"time"
 
+	"github.com/deluan/navidrome/log"
 	"github.com/deluan/navidrome/model"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 //CloudScanner allows scanning of online resources
@@ -13,12 +16,23 @@ type CloudScanner interface {
 	Login(ID, Key string) error
 }
 
+type s3Client interface {
+	ListBuckets(ctx context.Context) ([]minio.BucketInfo, error)
+}
+
 //S3CloudScanner scans S3 storage
-type S3CloudScanner struct{}
+type S3CloudScanner struct {
+	s3Client s3Client
+}
 
 //NewS3CloudScanner returns an initialized S3CloudScanner
-func NewS3CloudScanner(rootFolder string, ds model.DataStore) CloudScanner {
-	return &S3CloudScanner{}
+func NewS3CloudScanner(rootFolder string, ds model.DataStore) *S3CloudScanner {
+	s := S3CloudScanner{}
+	err := s.Login("AKIAIOSFODNN7EXAMPLE", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
+	if err != nil {
+		log.Error(err)
+	}
+	return &s
 }
 
 //Scan scans a media folder in the cloud
@@ -27,6 +41,10 @@ func (s *S3CloudScanner) Scan(ctx context.Context, lastModifiedSince time.Time) 
 }
 
 //Login logs into the cloud storage
-func (s *S3CloudScanner) Login(ID, Key string) error {
-	return nil
+func (s *S3CloudScanner) Login(id, key string) (err error) {
+	s.s3Client, err = minio.New("localhost:9000", &minio.Options{
+		Creds:  credentials.NewStaticV4(id, key, ""),
+		Secure: false,
+	})
+	return
 }
